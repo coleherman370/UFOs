c5b567f2a13015f023196b04756b01359feec59c
Cole Herman <cole.herman370@outlook.com>
2021-12-08 23:25:53 -0600

    Update app.js to include listener and filter functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -17,3 +23,31 @@ function buildTable(data) {
       );
     });
   }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+// Listen for the button to be clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build unfiltered table upon loading of page
+buildTable(tableData);
+
+
+
